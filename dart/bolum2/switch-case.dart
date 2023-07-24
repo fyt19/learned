9aaebed0522f5aa0479434dfbe61c2984d39c9e7
furkan yuksel <99129857+fyt19@users.noreply.github.com>
2023-07-24 21:27:53 +0300

    Create switch-case.dart

---
+++ dart/bolum2/switch-case.dart
@@ -0,0 +1,41 @@
+void main(List<String> args) {
+  String not = 'AA';
+  switch (not) {
+    case "AA":
+      print("notunuz 90-100 arasındadır");
+      break;
+
+    case "BA":
+      print("notunuz 80-90 arasıdır.");
+      break;
+
+    case "BB":
+      print("notunuz 70-80 arasındadır");
+      break;
+
+    case "CB":
+      print("notunuz 60-70 arasıdır.");
+      break;
+
+    case "CC":
+      print("notunuz 50-60 arasıdır.");
+      break;
+
+    case "DC":
+      print("notunuz 40-50 arasındadır");
+      break;
+
+    case "DD":
+      print("notunuz 30-40 arasıdır.");
+      break;
+
+    case "FF":
+      print("notunuz 20-30 arasıdır.");
+      break;
+
+    default:
+      {
+        print("hatalı bir tuşlama yapıldı");
+      }
+  }
+}
