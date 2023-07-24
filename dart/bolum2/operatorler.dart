e5c53efb198401f563c4ebe81ed247e40ea46874
furkan yuksel <99129857+fyt19@users.noreply.github.com>
2023-07-24 21:27:32 +0300

    Create operatorler.dart

---
+++ dart/bolum2/operatorler.dart
@@ -0,0 +1,42 @@
+void main(List<String> args) {
+  double sayi = 9;
+  double sayi2 = 6;
+
+  /*
+  aritmatik işlemler
+  +, toplama
+  -, çıkarma
+  *, çarpma
+  /, bölme
+  %, mod
+  <, küçüktür
+  >, büyüktür
+  <=, küçük eşit
+  >=, büyük eşit
+  ==, eşit eşit
+  != eşit değilse
+  mantıksal op.
+  &&, ve (1,1)
+  ||, veya (1,0)
+  !, değil
+  arttırma azaltma
+  ++, arttırma
+  --, azaltma
+  */
+
+  int sayi1 = 10;
+  sayi1 = sayi1 + 1;
+  sayi1 += 5;
+  print(sayi1);
+  sayi1++;
+  print(sayi1);
+  int sayi3 = 10;
+  print(sayi3++);
+  print(++sayi3);
+  /*
+  işlem önceliği
+  parantez içi çarpma toplama
+
+
+  */
+}
